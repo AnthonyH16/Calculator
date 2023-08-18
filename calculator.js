06dc64183050eb169559d99261f9d74c979730ba
AnthonyH16 <132026902+AnthonyH16@users.noreply.github.com>
2023-08-18 11:24:28 -0400

    create calculation functions create operate function

---
+++ calculator.js
@@ -0,0 +1,32 @@
+let first = 0;
+let operator
+let second = 0;
+function operate(){
+
+};
+
+function split(){
+
+};
+
+function add (a,b) {
+    return a + b;
+  };
+
+  
+function subtract(a, b) {
+    return a - b;
+  };
+
+
+function multiply(...nums) {
+    let total = 1;
+    for (i = 0; i < nums.length; i++) {
+      total *= nums[i];
+    };
+    return total;
+  }
+
+function divide (a,b) {
+    return a / b ;
+  }
